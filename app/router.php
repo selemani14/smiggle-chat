49b1ffaadd7330e6db2f27bc7d5373dd199244e0
selemani14 <demuenator@gmail.com>
2016-06-22 06:58:10 +0200

    Update router.php

--- app/router.php
+++ app/router.php
@@ -8,14 +8,20 @@ class router{
 
 
   public function gets()
-	{
-	  //var_dump($this->getter['smiggle']); die();
-	  $url_part = explode('/', $this->getter['smiggle']);
-
-	  $controller = $url_part[0];
-	  $action = $url_part[1];
+	{ 
+	  if(isset($this->getter['smiggle']))
+	  {
+		  $url_part = explode('/', $this->getter['smiggle']);
+		  $controller = $url_part[0];
+		  $action = $url_part[1];
+	  }
+	  else
+	  {
+	  	$controller = "index";
+		$action = "index";
+		  
+	  }
 	  $class = $controller. 'controller';
-	  
 	  $controller = new $class();
 	  $controller->$action();
 	}
