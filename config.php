a0eab8adeab79a854e71cda2d6883b9b6b3b6f59
selemani14 <demuenator@gmail.com>
2016-06-22 06:28:19 +0200

    Update config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'chat');
 
 /** MySQL database username */
-define('DB_USER', 'roots');
+define('DB_USER', 'root');
 
 /** MySQL database password */
 define('DB_PASSWORD', '');
