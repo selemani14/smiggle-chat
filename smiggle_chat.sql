bf022f07252e409ec83ae10e9baf836cc32fce42
selemani14 <demuenator@gmail.com>
2016-06-22 06:21:56 +0200

    Add files via upload

---
+++ smiggle_chat.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 22, 2016 at 02:29 AM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `chat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unread_messages`
+--
+
+CREATE TABLE IF NOT EXISTS `unread_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sent_from` int(11) NOT NULL,
+  `sent_to` int(11) NOT NULL,
+  `unread_messages` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `unread_messages`
+--
+
+INSERT INTO `unread_messages` (`id`, `sent_from`, `sent_to`, `unread_messages`) VALUES
+(1, 1, 2, 0),
+(2, 1, 3, 0),
+(3, 1, 4, 0),
+(4, 2, 1, 0),
+(5, 2, 3, 0),
+(6, 2, 4, 0),
+(7, 3, 1, 0),
+(8, 3, 2, 0),
+(9, 3, 4, 0),
+(10, 4, 1, 0),
+(11, 4, 2, 0),
+(12, 4, 3, 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
