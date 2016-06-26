95eccdec107ad7108ef9a752f1cd5c1865a53d42
selemani14 <demuenator@gmail.com>
2016-06-26 22:28:29 +0200

    Update user_status.php

--- views/user_status.php
+++ views/user_status.php
@@ -1,14 +1,14 @@
 <?php
 if($row[0]['login_status'] == 'off')
 		{
-			$chat_status ="<font color='#fff'>Status:</font> <font color='#cdc108'>Start chatting..</font>";
-			$log_status ="<font color='#cdc108'>offline</font>";
+			$chat_status ="<de class='label label-danger' style='font-size:11px;'><font color='#fff'>Status:</font> <font color='#fff'>Start chatting..</font></de><br/>";
+			$log_status ="<de class='label label-danger' style='font-size:11px;'><font color='#fff'>offline</font></de><br/>";
 			echo "$log_status <br/> $chat_status";
 		}
 		else
 		{
-			$chat_status = "<font color='#fff'>Status:</font> <font color='#77ec88'>continue chatting..</font>";
-			$log_status ="<font color='#77ec88'>online</font>";
+			$chat_status = "<de class='label label-success' style='font-size:11px;'><font color='#fff'>Status:</font> <font color='#fff'>continue chatting..</font></de><br/>";
+			$log_status ="<de class='label label-success' style='font-size:11px;'><font color='#fff'>online</font></de><br/>";
 			echo "$log_status <br/> $chat_status";
 		}
 ?>
